a6ca63da275dc1239445ee8d2c124a517e80bbab
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-06-11 19:59:55 +0000

    Update modding-api.js

--- modding-api.js
+++ modding-api.js
@@ -1,6 +1,7 @@
 class ModdingAPI {
     constructor() {
         this.mods = [];
+        this.loadModsFromStorage();
     }
 
     loadMod(mod) {
@@ -8,13 +9,13 @@ class ModdingAPI {
             this.mods.push(mod);
             mod.applyMod();
             console.log(`Mod "${mod.name}" version ${mod.version} loaded successfully.`);
+            this.saveModToStorage(mod);
         } else {
             console.error(`Failed to load mod "${mod.name}".`);
         }
     }
 
     validateMod(mod) {
-        // Basic validation
         if (!mod.name || !mod.version || typeof mod.applyMod !== 'function') {
             return false;
         }
@@ -27,6 +28,36 @@ class ModdingAPI {
 
     getMods() {
         return this.mods;
+    }
+
+    saveModToStorage(mod) {
+        let storedMods = JSON.parse(localStorage.getItem('mods')) || [];
+        storedMods.push(mod);
+        localStorage.setItem('mods', JSON.stringify(storedMods));
+    }
+
+    loadModsFromStorage() {
+        let storedMods = JSON.parse(localStorage.getItem('mods')) || [];
+        storedMods.forEach(mod => {
+            if (this.validateMod(mod)) {
+                this.mods.push(mod);
+            }
+        });
+        this.applyAllMods();
+    }
+
+    clearAllMods() {
+        this.mods = [];
+        localStorage.removeItem('mods');
+        console.log('All mods cleared from storage.');
+    }
+
+    removeMod(modName) {
+        this.mods = this.mods.filter(mod => mod.name !== modName);
+        let storedMods = JSON.parse(localStorage.getItem('mods')) || [];
+        storedMods = storedMods.filter(mod => mod.name !== modName);
+        localStorage.setItem('mods', JSON.stringify(storedMods));
+        console.log(`Mod "${modName}" removed.`);
     }
 }
 
